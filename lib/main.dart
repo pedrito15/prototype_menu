8923d09f6f9314251cc717ab58d76a7713ea1e28
Pedro Sanchez Arredondo <pedrosanchezarredondo@iMac-de-Pedro.local>
2020-07-06 17:04:33 -0500

    Se añadieron cambios

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
+//import 'dart:convert';
+
 import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_tab_bar_no_ripple/flutter_tab_bar_no_ripple.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
-import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
-import 'package:toggle_bar/toggle_bar.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -32,10 +31,133 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Ejemple'),
+      home: MyHomePage(title: 'Ejemplo'),
     );
   }
 }
+
+  List _sections = [
+      {
+          "id": 0,
+          "name": "Hamburguesas",
+          "foods": [
+            {
+              "name" : "Wooper Jr", 
+              "description": "Rica hamburguesa !!!", 
+              "price" : 12.00,
+              "image": "assets/img/burguer.jpg"
+            },
+            {
+              "name" : "Famous Star", 
+              "description": "Hamburguesa más grande", 
+              "price" : 12.00,
+              "image": "assets/img/burguer2.jpeg"
+            },
+            {
+              "name" : "Tocino Burguer", 
+              "description": "Incluye rico tocino", 
+              "price" : 12.00,
+              "image": "assets/img/burguer3.jpeg"
+            },
+          ]
+      },
+      {
+          "id": 1,
+          "name": "Tacos",
+          "foods": [
+            {
+              "name" : "Deshebrada", 
+              "description": "Tacos deshebrada", 
+              "price" : 12.00,
+              "image": "assets/img/tacos1.jpg"
+            },
+            {
+              "name" : "Chicharron", 
+              "description": "", 
+              "price" : 12.00,
+              "image": "assets/img/tacos2.jpeg"
+            },
+            {
+              "name" : "Frijoles", 
+              "description": "", 
+              "price" : 12.00,
+              "image": "assets/img/tacos3.jpeg"
+            },
+          ]
+      },
+      {
+          "id": 2,
+          "name": "Bebidas",
+          "foods": [
+            {
+              "name" : "Seven Up", 
+              "description": "500ml", 
+              "price" : 12.00,
+              "image": "assets/img/sevenup.jpg"
+            },
+            {
+              "name" : "Coca Cola", 
+              "description": "600ml", 
+              "price" : 12.00,
+              "image": "assets/img/cocacola.jpeg"
+            },
+            {
+              "name" : "Fanta", 
+              "description": "700ml", 
+              "price" : 12.00,
+              "image": "assets/img/fanta.jpeg"
+            },
+          ]
+      },
+      {
+          "id": 3,
+          "name": "Tortas",
+          "foods": [
+            {
+              "name" : "Torta de pierna", 
+              "description": "Con aguacate", 
+              "price" : 12.00,
+              "image": "assets/img/tortapierna.jpg"
+            },
+            {
+              "name" : "Torta de jamón", 
+              "description": "Con queso", 
+              "price" : 12.00,
+              "image": "assets/img/tortajamon.jpeg"
+            },
+            {
+              "name" : "Torta de Bistec", 
+              "description": "Con lechuga, aguacate y queso", 
+              "price" : 12.00,
+              "image": "assets/img/tortabistec.jpg"
+            },
+          ]
+      },
+      {
+          "id": 4,
+          "name": "Pasteles",
+          "foods": [
+            {
+              "name" : "Pastel de Yogut", 
+              "description": "Relleno de yogurt", 
+              "price" : 150.00,
+              "image": "assets/img/pastelyogurt.jpg"
+            },
+            {
+              "name" : "Paste de Chocolate", 
+              "description": "Cubierto de chocolate", 
+              "price" : 160.00,
+              "image": "assets/img/pastelchoco.jpg"
+            },
+            {
+              "name" : "Pastel de Bombon", 
+              "description": "Dulces bombones", 
+              "price" : 170.00,
+              "image": "assets/img/pastelbombon.jpg"
+            },
+          ]
+      }
+  ];
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -55,35 +177,34 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+
 class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin{
 
   int currentIndex = 0;
 
-  final  posLimit = 1753;
   int    tabIndex = 0;
-  double posInitial = 0;
+  double tabBarHeight = 0;
 
-  ScrollController _scrollController2 = new ScrollController();
+  //List<double> positions = [290.0, 648.0, 1006.0];  // Posiciones de scroll de cada sección
+  List<double> positions = new List<double>();
+
+  ScrollController _scrollController = new ScrollController();
   TabController _tabController;
-
-  final ItemScrollController itemScrollController = ItemScrollController();
-  final ItemPositionsListener itemPositionsListener = ItemPositionsListener.create();
 
   bool isAnimated = false;
 
-
-  EdgeInsetsGeometry titlePadding =  EdgeInsetsDirectional.only(bottom: 0);
+  double labelHeight = 20.0;
+  double cardHeight  = 100.0;
 
   Color theme = Colors.white;
 
   void scrollToOffset(double offset) {
-    //var y = offset + _scrollController2.position.pixels; // Desde la posicion en la que estoy actualmente
+    //var y = offset + _scrollController.position.pixels; // Desde la posicion en la que estoy actualmente
 
     isAnimated = true;
-    posInitial = offset; // Actualizar posición 
 
     var y = offset;
-    _scrollController2.animateTo(
+    _scrollController.animateTo(
       y,
       duration: Duration(milliseconds: 250),
       curve: Curves.ease,
@@ -94,29 +215,30 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
   void initState() { 
     super.initState();
 
-    _tabController = new TabController(length: 6, vsync: this);
+    _tabController = new TabController(length: _sections.length, vsync: this);
+    _scrollController.addListener(() {
 
-    _scrollController2.addListener(() {
+      if(!isAnimated){
 
-      if(!isAnimated){  // Si no esta animado entonces poder cambiar 'tabbar'
+        // Si el scroll esta dentro del limite
+        if(_tabController.index > 0){ // Hacia arriba
+          if(_scrollController.position.pixels <= positions[_tabController.index - 1]){ 
+          
+            _tabController.index--;
+          }
+        }
 
-        print('ScrollPosition: ${_scrollController2.position.pixels}');
+        if(_tabController.index < _tabController.length - 1){ // Hacia abajo
 
-        if(_scrollController2.position.pixels > posInitial + posLimit){
+          if(_scrollController.position.pixels >= positions[_tabController.index + 1] - 100){
 
-        _tabController.index++;
-        posInitial = (posLimit * _tabController.index).toDouble();
-
-
-        }else if(_scrollController2.position.pixels < posInitial){
-
-          _tabController.index--;
-          posInitial = (posLimit * _tabController.index).toDouble();
-
+            _tabController.index++;
+          }
         }
       }
-    });
 
+       print('ScrollPosition: ${_scrollController.position.pixels}');
+    });
   }
 
 
@@ -125,16 +247,17 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
    
     return Scaffold(
       body: DefaultTabController(
-        length: 6,
+        length: _tabController.length,
         child: CustomScrollView(
-          controller: _scrollController2,
+          physics: new ClampingScrollPhysics(), // Evitar que rebote el scroll
+          controller: _scrollController,
           slivers: <Widget>[
 
             // Encabezado de la aplicación
             _crearAppbar(),
 
             SliverPersistentHeader(
-              delegate: _SliverAppBarDelegate2(
+              delegate: _SliverAppBarRestaurantInfo(
                 minExtent: 100,
                 maxExtent: 100
               ),
@@ -143,69 +266,19 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
 
             SliverPersistentHeader(
               delegate: _SliverAppBarDelegate(
-                TabBar(
-                  onTap: (index){
-                    
-                    scrollToOffset(1753.0 * index);
-                    //print('Haz hecho click en el botón $index');
-                  },
-                  controller: _tabController,
-                  indicatorSize: TabBarIndicatorSize.tab,
-                  indicator: new BubbleTabIndicator(
-                    indicatorHeight: 26.0,
-                    indicatorColor: Colors.black,
-                    tabBarIndicatorSize: TabBarIndicatorSize.tab,
-                  ),
-                  isScrollable: true,
-                  unselectedLabelColor: Colors.black,
-                  indicatorColor: Colors.white,
-                  tabs: [
-                    Tab(
-                      child: Text('Seleccionado para ti'),
-                    ),
-                    Tab(
-                      child: Text('Tacos'),
-                    ),
-                    Tab(
-                      child: Text('Hamburguesas'),
-                    ),
-                    Tab(
-                      child: Text('Tortas'),
-                    ),
-                    Tab(
-                      child: Text('Burritos'),
-                    ),
-                    Tab(
-                      child: Text('Bebidas'),
-                    )
-                  ]
-                ),
+                _getTabBar()
               ),
               pinned: true,
             ),
 
             // Contenido del cuerpo de la aplicación
-            SliverFixedExtentList(
-              itemExtent: 150.0,
+            SliverList(
               delegate: SliverChildListDelegate(
-                [
-                  for(var i = 0; i < 100; i++)
-                  Card(
-                    color: i % 2 == 0 ? Colors.yellow : Colors.green,
-                    child: Container(
-                      alignment: Alignment.center,
-                      width: double.infinity,
-                      height: 100.0,
-                      child: Text(
-                        'Food Container $i',
-                        style: TextStyle(fontSize: 18.0),
-                      ),
-                    ),
-                  )
-                  
-                ],
+                  // Cargar 'Widgets'
+                  _getMenu()
               ),
-            ),
+            )
+
           ]
         )
 
@@ -214,7 +287,130 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
   }
 
 
-Widget _crearAppbar(){
+TabBar _getTabBar(){
+
+  var tabBar = TabBar(
+    onTap: (index){
+
+      scrollToOffset(positions[index]);
+      //print('Haz hecho click en el botón $index');
+    },
+    controller: _tabController,
+    indicatorSize: TabBarIndicatorSize.tab,
+    indicator: new BubbleTabIndicator(
+      indicatorHeight: 27.0,
+      indicatorColor: Colors.black,
+      tabBarIndicatorSize: TabBarIndicatorSize.tab,
+    ),
+    isScrollable: true,
+    unselectedLabelColor: Colors.black,
+    indicatorColor: Colors.white,
+    tabs:
+    [
+      for(var i = 0; i < _tabController.length; i++)
+      Tab(
+        child: Text(_sections[i]['name']),
+      )
+    ]
+  );
+
+  tabBarHeight = tabBar.preferredSize.height; // Obtener la altura del 'tabBar'
+  //print("tabBarHeight: $tabBarHeight");
+
+  return tabBar;
+}
+
+  List<Widget> _getMenu(){
+    final children = <Widget>[];
+    double position = 0;
+
+    // Agregar la posición
+    double appBarHeight = 290;
+    position += appBarHeight;
+    
+    positions.add(position);
+
+    for(var i = 0; i < _sections.length; i++){
+
+      children.add(
+        Container(
+          padding: const EdgeInsets.only(
+            left: 15
+          ),
+          child: Text(
+            _sections[i]['name'],
+            textAlign: TextAlign.left,
+            style: TextStyle(fontSize: labelHeight),
+          )
+        )
+      );
+
+      position += tabBarHeight;
+      position += labelHeight;
+
+      for(var j = 0; j < _sections[i]['foods'].length; j++){
+                        
+        children.add(
+
+          Card(
+            //color: j % 2 == 0 ? Colors.yellow : Colors.green,
+            child: Container(
+              margin: EdgeInsets.only(bottom: 5.0),
+              alignment: Alignment.center,
+              width: double.infinity,
+              height: cardHeight,
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(left: 9,right: 9),
+                    child:  ClipRRect(
+                      child: Image.asset(_sections[i]['foods'][j]['image'], width: 80, height: 80),
+                    ),
+                  ),
+                 
+                  Flexible(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        SizedBox(height: 8),
+                        Text(_sections[i]['foods'][j]['name'], style: TextStyle(fontSize: 18.0)),
+                        SizedBox(height: 6),
+                        Text(_sections[i]['foods'][j]['description'], style: TextStyle(fontSize: 14.0, color: Colors.grey)),
+                        SizedBox(height: 6),
+                        Text('\$${_sections[i]['foods'][j]['price'].toStringAsFixed(2)}', style: TextStyle(fontSize: 17.0)),
+                      ],
+                      
+                    )
+                  )
+
+                  
+                ],
+              )
+              
+            ),
+          )
+        );
+
+        position += (cardHeight + 5); // 5 es el margen de Card
+      }
+
+      children.add(
+        Divider(thickness: 2.5,)
+      );
+
+      position-=3; // Es el grueso del divider
+
+      positions.add(position); // Agregar la posición del widget en la lista
+    }
+
+    print('$positions');
+                                    
+    return children;
+  }
+
+
+  Widget _crearAppbar(){
 
     return SliverAppBar(
       actions: <Widget>[
@@ -230,7 +426,6 @@ Widget _crearAppbar(){
           style: TextStyle(fontSize: 18.0),
           textAlign: TextAlign.left,
         ),
-        //titlePadding: titlePadding,  // Para quitar el pading del titulo
         collapseMode: CollapseMode.pin,
         background:  Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -255,16 +450,11 @@ Widget _crearAppbar(){
 }
 
 
-  class _SliverAppBarDelegate2 extends SliverPersistentHeaderDelegate {
-    _SliverAppBarDelegate2({this.minExtent, this.maxExtent});
+  class _SliverAppBarRestaurantInfo extends SliverPersistentHeaderDelegate {
+    _SliverAppBarRestaurantInfo({this.minExtent, this.maxExtent});
 
     final double minExtent;
     final double maxExtent;
-
-    @override
-    //double get minExtent => _myWidget.preferredSize.height;
-    @override
-    //double get maxExtent => _myWidget.preferredSize.height;
 
     @override
     Widget build(
@@ -288,7 +478,7 @@ Widget _crearAppbar(){
     }
 
     @override
-    bool shouldRebuild(_SliverAppBarDelegate2 oldDelegate) {
+    bool shouldRebuild(_SliverAppBarRestaurantInfo oldDelegate) {
       return false;
     }
   }
@@ -306,6 +496,7 @@ Widget _crearAppbar(){
     @override
     Widget build(
         BuildContext context, double shrinkOffset, bool overlapsContent) {
+
       return new Container(
           color: Colors.white,
           child: _tabBar,
